cc669411bcb9452cc8ea28995f176c3aabf59e71
Anna <annaspacer7@gmail.com>
2024-12-04 14:58:19 +0300

    feat: add isPositive solution

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,14 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  if (number === 0) {
+    return true;
+  }
+  if (number > 0) {
+    return true;
+  }
+  return false;
 }
 
 /**
